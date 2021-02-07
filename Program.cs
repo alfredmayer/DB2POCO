ba64c42ddfcdafa46e0793f95a801d61a6725ff5
alfred.mayer@namk.com <alfred.mayer@namk.com>
2021-02-07 15:02:27 +0100

    First Steps

--- Program.cs
+++ Program.cs
@@ -12,8 +12,12 @@ namespace DB2POCO
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (args.Length == 1 && "test" == args[0]) {
+                Application.Exit();
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
